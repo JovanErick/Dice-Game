c6e3d6af5c1e56fd104cc7fa73d9de4a50ff5588
JovanErick <jovanerick.coding@gmail.com>
2023-07-06 16:56:36 +0700

    commit2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variable Declaration
 let inputPlayerName1 = prompt("Enter the first player's name");
 let inputPlayerName2 = prompt("Enter the second player's name");
 const playerName1 = document.querySelector('.dice--1 h2');
@@ -6,6 +7,7 @@ const diceImage1 = document.querySelector('.dice--1 img');
 const diceImage2 = document.querySelector('.dice--2 img');
 const winner = document.querySelector('main p');
 
+// Change player name
 if (inputPlayerName1 !== '') {
 	playerName1.innerHTML = inputPlayerName1;
 }
@@ -14,58 +16,29 @@ if (inputPlayerName2 !== '') {
 	playerName2.innerHTML = inputPlayerName2;
 }
 
-if (inputPlayerName1 === '' || inputPlayerName1 === null) {
+if (
+	inputPlayerName1 === '' ||
+	inputPlayerName1 === null ||
+	inputPlayerName2 === '' ||
+	inputPlayerName2 === null
+) {
 	playerName1.innerHTML = 'Player 1';
 	playerName2.innerHTML = 'Player 2';
 }
 
-if (inputPlayerName2 === '' || inputPlayerName2 === null) {
-	playerName1.innerHTML = 'Player 1';
-	playerName2.innerHTML = 'Player 2';
-}
-
+// Dice roll
 function rollDice() {
+	// Variable declaration
 	let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 	let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+	let diceImageUpdate1 = 'images/dice' + randomNumber1 + '.png';
+	let diceImageUpdate2 = 'images/dice' + randomNumber2 + '.png';
 
-	if (randomNumber1 === 1) {
-		diceImage1.setAttribute('src', 'images/dice1.png');
-	}
-	if (randomNumber1 === 2) {
-		diceImage1.setAttribute('src', 'images/dice2.png');
-	}
-	if (randomNumber1 === 3) {
-		diceImage1.setAttribute('src', 'images/dice3.png');
-	}
-	if (randomNumber1 === 4) {
-		diceImage1.setAttribute('src', 'images/dice4.png');
-	}
-	if (randomNumber1 === 5) {
-		diceImage1.setAttribute('src', 'images/dice5.png');
-	}
-	if (randomNumber1 === 6) {
-		diceImage1.setAttribute('src', 'images/dice6.png');
-	}
+	// Update dice image	
+	diceImage1.setAttribute('src', diceImageUpdate1);
+	diceImage2.setAttribute('src', diceImageUpdate2);
 
-	if (randomNumber2 === 1) {
-		diceImage2.setAttribute('src', 'images/dice1.png');
-	}
-	if (randomNumber2 === 2) {
-		diceImage2.setAttribute('src', 'images/dice2.png');
-	}
-	if (randomNumber2 === 3) {
-		diceImage2.setAttribute('src', 'images/dice3.png');
-	}
-	if (randomNumber2 === 4) {
-		diceImage2.setAttribute('src', 'images/dice4.png');
-	}
-	if (randomNumber2 === 5) {
-		diceImage2.setAttribute('src', 'images/dice5.png');
-	}
-	if (randomNumber2 === 6) {
-		diceImage2.setAttribute('src', 'images/dice6.png');
-	}
-
+	// Winner of the game
 	if (randomNumber1 > randomNumber2) {
 		winner.innerHTML =
 			'<strong>The winner is:</strong><br>' + playerName1.textContent;
@@ -75,4 +48,8 @@ function rollDice() {
 	} else {
 		winner.innerHTML = '<strong>The result is a draw</strong>';
 	}
+}
+
+function newGame() {
+    location.reload();
 }
